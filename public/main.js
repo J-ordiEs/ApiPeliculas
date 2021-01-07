46286e3d008b443cfe516be2ad8eb446ba2c9e28
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-01-07 17:23:50 +0100

    finish crud

--- public/main.js
+++ public/main.js
@@ -5,6 +5,7 @@ const imgValue = document.getElementById("urlImage-value");
 const dirValue = document.getElementById("director-value");
 const sinpValue = document.getElementById("sinopsis-value");
 const clasValue = document.getElementById("clasification-value");
+const btnSubmit = document.querySelector(".btnS");
 
 let output = "";
 
@@ -15,8 +16,9 @@ const renderMovies = (movies) => {
               <div class="card-body" data-id=${movie.id}>
                 <img src="${movie.imagen}" class="card-img-top mt-2" alt="Image Movie" />
                 <h5 class="card-title">${movie.nombre}</h5>
-                <p class="card-text">${movie.director}</p>
-                <p class="card-text2">${movie.sinopsis}</p>
+                <p class="card-text1">${movie.clasificacion}</p>
+                <p class="card-text2">${movie.director}</p>
+                <p class="card-text3">${movie.sinopsis}</p>
                 <a href="#" class="btn btn-secondary bg-dark" id="editMovie">Edit</a>
                 <a href="#" class="btn btn-secondary bg-dark" id="delMovie">Delete</a>
               </div>
@@ -53,10 +55,39 @@ movieList.addEventListener("click", (e) => {
 
     if (editBtnIsPress) {
         const parent = e.target.parentElement;
-        let imgContent = parent.querySelector(".card-img-top").;
         let movieContent = parent.querySelector(".card-title").textContent;
-        console.log(movieContent, imgContent);
+        let imgContent = parent.querySelector(".card-img-top").src;
+        let dirContent = parent.querySelector(".card-text2").textContent;
+        let sinpContent = parent.querySelector(".card-text3").textContent;
+        let clasContent = parent.querySelector(".card-text1").textContent;
+
+        movieValue.value = movieContent;
+        imgValue.value = imgContent;
+        dirValue.value = dirContent;
+        sinpValue.value = sinpContent;
+        clasValue.value = clasContent;
     }
+
+    //Update -update the existing movie
+    // Method: PATCH
+    btnSubmit.addEventListener("click", (e) => {
+        e.preventDefault();
+        fetch(`${url}/${id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    nombre: movieValue.value,
+                    imagen: imgValue.value,
+                    director: dirValue.value,
+                    sinopsis: sinpValue.value,
+                    clasificacion: clasValue.value,
+                }),
+            })
+            .then((res) => res.json())
+            .then(() => location.reload());
+    });
 });
 
 // Create - Insert new movie
@@ -81,4 +112,9 @@ addMovieForm.addEventListener("submit", () => {
             dataArr.push(data);
             renderMovies(dataArr);
         });
+    movieValue.value = "";
+    imgValue.value = "";
+    dirValue.value = "";
+    sinpValue.value = "";
+    clasValue.value = "";
 });
