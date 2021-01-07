6c50631953f62a5b53ac562e06810970d15bd828
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-01-07 15:27:19 +0100

    edit del

--- public/main.js
+++ public/main.js
@@ -1,5 +1,10 @@
 const movieList = document.querySelector(".movie-list");
 const addMovieForm = document.querySelector(".add-movie-form");
+const movieValue = document.getElementById("movie-value");
+const imgValue = document.getElementById("urlImage-value");
+const dirValue = document.getElementById("director-value");
+const sinpValue = document.getElementById("sinopsis-value");
+const clasValue = document.getElementById("clasification-value");
 
 let output = "";
 
@@ -7,14 +12,14 @@ const renderMovies = (movies) => {
     movies.forEach((movie) => {
         output += `
             <div class="card mt-4 col-md-6">
-                <img src="${movie.imagen}" class="card-img-top mt-2" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">Title: ${movie.nombre}</h5>
-                    <p class="card-text">Director: ${movie.director}</p>
-                    <p class="card-text">Sinopsis: ${movie.sinopsis}</p>
-                    <a href="#" class="btn btn-secondary bg-dark">Edit</a>
-                    <a href="#" class="btn btn-secondary bg-dark">Delete</a>
-                </div>
+              <div class="card-body" data-id=${movie.id}>
+                <img src="${movie.imagen}" class="card-img-top mt-2" alt="Image Movie" />
+                <h5 class="card-title">${movie.nombre}</h5>
+                <p class="card-text">${movie.director}</p>
+                <p class="card-text2">${movie.sinopsis}</p>
+                <a href="#" class="btn btn-secondary bg-dark" id="editMovie">Edit</a>
+                <a href="#" class="btn btn-secondary bg-dark" id="delMovie">Delete</a>
+              </div>
             </div>
             `;
     });
@@ -30,8 +35,50 @@ fetch(url)
     .then((res) => res.json())
     .then((data) => renderMovies(data));
 
+movieList.addEventListener("click", (e) => {
+    let delBtnIsPress = e.target.id == "delMovie";
+    let editBtnIsPress = e.target.id == "editMovie";
+
+    let id = e.target.parentElement.dataset.id;
+
+    // Delete - Remove the existing movie
+    // Method: DELETE
+    if (delBtnIsPress) {
+        fetch(`${url}/${id}`, {
+                method: "DELETE",
+            })
+            .then((res) => res.json())
+            .then(() => location.reload());
+    }
+
+    if (editBtnIsPress) {
+        const parent = e.target.parentElement;
+        let imgContent = parent.querySelector(".card-img-top").;
+        let movieContent = parent.querySelector(".card-title").textContent;
+        console.log(movieContent, imgContent);
+    }
+});
+
 // Create - Insert new movie
 //Method: POST
 addMovieForm.addEventListener("submit", () => {
-    console.log("form submited");
+    fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                nombre: movieValue.value,
+                imagen: imgValue.value,
+                director: dirValue.value,
+                sinopsis: sinpValue.value,
+                clasificacion: clasValue.value,
+            }),
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            const dataArr = [];
+            dataArr.push(data);
+            renderMovies(dataArr);
+        });
 });
